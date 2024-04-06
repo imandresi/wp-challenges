59fe31ce894d0e0bc13098ce2052016b133d7629
imandresi <contact@mandresi.net>
2024-04-06 19:21:07 +0300

    (feat:challenge_01_01) Install @wordpress/create-block

---
+++ challenge_01/task_01/testimonial/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fd008544e5451a8e3221');
---
+++ challenge_01/task_01/testimonial/testimonial.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name:       Testimonial Card
+ * Description:       Example block scaffolded with Create Block tool.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            Tanjaka Mandresi
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       testimonial
+ * Domain Path:       imandresi-testimonial
+ *
+ * @package Imandresi
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function imandresi_testimonial_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'imandresi_testimonial_block_init' );
