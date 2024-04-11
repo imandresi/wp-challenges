731a63847fa9c042c24b34f63eef6dd044a85bc1
imandresi <contact@mandresi.net>
2024-04-11 18:52:45 +0300

    (fix:challenge_01_01) arrange css styling on avatar

--- challenge_01/task_01/testimonial-card/build/index.asset.php
+++ challenge_01/task_01/testimonial-card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '71565d2f1f4ed5fb6747');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0764eceafd9239cb9711');
