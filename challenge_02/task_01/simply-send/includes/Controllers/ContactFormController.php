c92c82473cb58bf200a0e743f8c790f19a71625f
imandresi <contact@mandresi.net>
2024-05-24 12:57:53 +0300

    (feat:challenge_02_01) handle form submission

--- challenge_02/task_01/simply-send/includes/Controllers/ContactFormController.php
+++ challenge_02/task_01/simply-send/includes/Controllers/ContactFormController.php
@@ -7,16 +7,93 @@ use Imandresi\SimplySend\Views\ContactFormView;
 use const Imandresi\SimplySend\PLUGIN_SHORTCODE_NAME;
 
 class ContactFormController extends Singleton {
+	private const NONCE_FIELD = '_wpnonce';
+	private const NONCE_ACTION = 'submit';
+
 
 	public function process_shortcode( $atts ): string {
-		$attributes = [ 'name' => 'Tanjaka Mandresi' ];
+		// put field values here
+		$attributes = [
+			'name'   => 'Tanjaka Mandresi',
+			'nonce'  => wp_nonce_field( self::NONCE_ACTION, self::NONCE_FIELD ),
+			"errors" => [
+				'subject' => 'That field is required.'
+			]
+		];
 
 		return ContactFormView::show_contact_form( $attributes );
 
 	}
 
+	public function submit_form() {
+
+		$form_state = [
+			'status'         => '',
+			'status_message' => '',
+			'form_data'      => $_POST,
+			'errors'         => [],
+		];
+
+		$safe_data = [];
+
+		// verify nonce
+		if ( ! wp_verify_nonce( $_POST[ self::NONCE_FIELD ], self::NONCE_ACTION ) ) {
+			wp_die( 'Sorry! You are not allowed to send that message.' );
+		}
+
+		// data sanitization
+		$safe['subject'] = trim( sanitize_text_field( $_POST['subject'] ) );
+		$safe['email']   = trim( sanitize_email( $_POST['email'] ) );
+		$safe['message'] = trim( sanitize_textarea_field( $_POST['message'] ) );
+
+		// data validation
+		if ( ! $safe['subject'] ) {
+			$form_state['errors']['subject'] = 'The subject of the message is required.';
+		}
+
+		if ( ! is_email( $safe['email'] ) ) {
+			$form_state['errors']['email'] = 'The email address is not valid.';
+		}
+
+		if ( ! $safe['message'] ) {
+			$form_state['errors']['message'] = 'Please type the message you want to send';
+		}
+
+		// preparing data to be returned
+		if ( $form_state['errors'] ) {
+			$form_state['status']         = 'errors';
+			$form_state['status_message'] = 'An error occured. Please verify your fields.';
+		} else {
+
+			// send the email
+			$to = $safe['email'];
+			$subject = $safe['subject'];
+			$message = $safe['message'];
+			$mail_status = wp_mail($to, $subject, $message);
+
+			if ( $mail_status ) {
+				$form_state['status']         = 'success';
+				$form_state['status_message'] = 'Your message is sent successfully';
+			} else {
+				$form_state['status']         = 'errors';
+				$form_state['status_message'] = 'The message could not be sent.';
+			}
+
+		}
+
+		set_transient( 'simply_send_form_state', $form_state, 120 );
+
+		// return to the form
+		$redirect_url = $_SERVER['HTTP_REFERER'];
+		wp_redirect( $redirect_url );
+		exit;
+
+	}
+
 	public function init() {
 		add_shortcode( PLUGIN_SHORTCODE_NAME, [ $this, 'process_shortcode' ] );
+		add_action( 'admin_post_simply_send_form_submit', [ $this, 'submit_form' ] );
+		add_action( 'admin_post_nopriv_simply_send_form_submit', [ $this, 'submit_form' ] );
 	}
 
 	public static function load() {
--- challenge_02/task_01/simply-send/includes/Views/ContactFormView.php
+++ challenge_02/task_01/simply-send/includes/Views/ContactFormView.php
@@ -13,6 +13,11 @@ class ContactFormView extends Singleton {
 
 	public static function show_contact_form( $attributes ): string {
 		$instance = self::get_instance();
+		$attributes = array_merge($attributes, [
+			'unique_id' => uniqid(),
+			'form_action_url' => admin_url('admin-post.php')
+		]);
+
 		$output   = $instance->twig->render( 'contact-form/shortcode.html.twig', $attributes );
 
 		return $output;
@@ -22,10 +27,6 @@ class ContactFormView extends Singleton {
 		$instance       = self::get_instance();
 		$instance->twig = new Environment( new FilesystemLoader( PLUGIN_TEMPLATES_DIR ) );
 
-	}
-
-	public static function load() {
-		self::get_instance();
 	}
 
 }
