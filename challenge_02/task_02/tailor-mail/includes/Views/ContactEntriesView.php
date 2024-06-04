76edcca183d63a972a600eb9b23434ad1aa70441
imandresi <contact@mandresi.net>
2024-06-04 07:21:10 +0300

    (feat:challenge_02_02) add owner and date when displaying contact entries

--- challenge_02/task_02/tailor-mail/includes/Views/ContactEntriesView.php
+++ challenge_02/task_02/tailor-mail/includes/Views/ContactEntriesView.php
@@ -3,6 +3,7 @@
 namespace Imandresi\TailorMail\Views;
 
 use Imandresi\TailorMail\Models\ContactEntriesModel;
+use Imandresi\TailorMail\Models\ContactFormsModel;
 use Imandresi\TailorMail\Views\AbstractView;
 
 class ContactEntriesView extends AbstractView {
@@ -11,15 +12,31 @@ class ContactEntriesView extends AbstractView {
 		$post_id = $post->ID;
 		$entry   = ContactEntriesModel::get_entry( $post_id );
 
-		if (!$entry['mail']['custom']) {
+		if ( ! $entry['mail']['custom'] ) {
 			return;
 		}
 
-		$custom_fields = $entry['mail']['custom'];
+		// get info on owner
+		$owner_id    = $entry['mail']['owner'];
+		$owner       = ContactFormsModel::get_data( $owner_id );
+		$owner_title = $owner['post']->post_title;
+		$owner_href  = add_query_arg( [
+			'post'   => $owner_id,
+			'action' => 'edit'
+		] );
+
+		$custom_fields            = $entry['mail']['custom'];
 		$custom_fields['content'] = $entry['post']['post_content'];
-		$output = self::render('contact-entries/meta-box-mail-fields.html.twig', [
-			'fields' => $custom_fields
-		]);
+		$custom_fields            = array_merge( [
+			'Date'         => $entry['post']['post_date']
+		], $custom_fields );
+		$output                   = self::render( 'contact-entries/meta-box-mail-fields.html.twig', [
+			'fields' => $custom_fields,
+			'owner'  => [
+				'title' => $owner_title,
+				'href'  => $owner_href
+			]
+		] );
 
 		print $output;
 
